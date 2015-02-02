61204d7ab4df4bf884441d2d3476c22c2392dae9
Apple <apple@AppledeMacBook-Pro-2.local>
2015-02-02 11:18:03 +0800

    Initial Commit

---
+++ TestProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestProject
+//
+//  Created by Apple on 15/2/2.
+//  Copyright (c) 2015年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
